e2cbad2babeb8701f915b7e81c0e21a25d19d745
KorneliaAL <104372863+KorneliaAL@users.noreply.github.com>
2022-07-09 10:16:42 +0200

    Weather condition using API

--- src/index.js
+++ src/index.js
@@ -22,5 +22,31 @@ function formatDate(time) {
   let fullDate = `${day} ${clock}`;
   return fullDate;
 }
+function displayWeather(response) {
+  document.querySelector("#current-city").innerHTML = response.data.name;
+  document.querySelector("#current-temp").innerHTML = Math.round(
+    response.data.main.temp
+  );
+  document.querySelector("#humidity").innerHTML = response.data.main.humidity;
+  document.querySelector("#wind-speed").innerHTML = Math.round(
+    response.data.wind.speed
+  );
+}
+function searchCity(city) {
+  let apiKey = "70f46e7b374a41958649bba5f746057f";
+  let unit = "metric";
+  let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=${unit}`;
+  axios.get(apiUrl).then(displayWeather);
+}
+
+
+function showCity(event) {
+  event.preventDefault();
+  let city = document.querySelector("#city-input").value;
+  let currentCity = document.querySelector("#current-city");
+  currentCity.innerHTML = `${city}`;
+  searchCity(city);
+}
+
 let date = document.querySelector("#weather-date");
 date.innerHTML = formatDate();
